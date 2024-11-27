787935852953aa0604edabc6844ef57447f575fd
Sathyanarayanan-ops <57038667+Sathyanarayanan-ops@users.noreply.github.com>
2024-11-27 12:03:49 -0500

    Cors and rest Fw changes in settings.py

--- backend/mapback/settings.py
+++ backend/mapback/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z^4!e#_b-u@x$!#%op+b1(m8w+-lb4d$s(8-zx!3od+=u%=i=e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,10 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rides',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
