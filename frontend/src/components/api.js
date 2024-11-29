0753d61eccecb247ca00abef6a67d4a769ce1e49
Sathyanarayanan-ops <57038667+Sathyanarayanan-ops@users.noreply.github.com>
2024-11-29 17:17:06 -0500

    CSRF fix , POST being sent in frontend, but getting GET in backend

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,9 +1,14 @@
 import axios from "axios";
+import Cookies from 'js-cookie';
 
+
+const csrfToken = Cookies.get("csrftoken");
 const apiClient = axios.create({
     baseURL: 'http://localhost:8000/api',
+    withCredentials:true,
     headers:{
         'Content-Type':'application/json',
+        ...(csrfToken && { "X-CSRFTOKEN":csrfToken}),
     },
 });
 
