bcc0135ac477b41064ae16ffb376309b1c30b70f
Sathyanarayanan-ops <57038667+Sathyanarayanan-ops@users.noreply.github.com>
2024-12-26 00:02:51 +0530

    Models changes

--- backend/rides/models.py
+++ backend/rides/models.py
@@ -1,19 +1,21 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField # Imported to have an array of string 
+from django.contrib.auth.models import User
+# Make use of Django user model to create a common user for both driver and rider 
+# These are linked using one to one 
 
 # Create your models here.
 class Rider(models.Model):
-    id = models.AutoField(primary_key=True)
-    email = models.EmailField(max_length=50, unique = True)
-    first_name = models.CharField(max_length = 30)
-    last_name = models.CharField(max_length=30)
-    phone = models.CharField(max_length=10)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="rider_profile")
     total_rides = models.IntegerField(default=0)
     miles = models.FloatField(default=0.0)
     amount_spent = models.FloatField(default=0.0)
     
+    def __str__(self):
+        return self.user.username
+    
 class Rides(models.Model):
-    ride_id = models.CharField(max_length=5, unique=True, blank=True,null=True)
+    ride_id = models.IntegerField(unique=True, blank=True, null=True)
     rider = models.ForeignKey(Rider,on_delete=models.CASCADE,related_name='ride_history')
     pickup = models.CharField(max_length=100)
     dropoff = models.CharField(max_length=100)
@@ -39,16 +41,16 @@ class Rides(models.Model):
     
 
 class Driver(models.Model):
-    id = models.AutoField(primary_key=True)
-    email = models.EmailField(max_length=20, unique = True)
-    first_name = models.CharField(max_length = 30)
-    last_name = models.CharField(max_length=30)
-    phone = models.CharField(max_length=10)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="driver_profile")
     rides = models.IntegerField(default=0)
     miles = models.FloatField(default=0.0)
     amount_earned = models.FloatField(default=0.0)
+    license_number = models.CharField(max_length=20, default="UNKNOWN")
     car_model = models.CharField()
     rating = models.FloatField(default=0.0)
+    
+    def __str__(self):
+        return self.user.username
     
 class Rating(models.Model):
     driver = models.ForeignKey(Driver, on_delete=models.CASCADE, related_name='ratings')
